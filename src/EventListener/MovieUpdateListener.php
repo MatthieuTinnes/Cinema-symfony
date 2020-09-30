5a205365233581c64cab7da0d9b3f170ca4f6ba8
Matthieu42 <matthieu.tinnes@gmail.com>
2020-09-30 14:48:19 +0200

    Add serializing

--- src/EventListener/MovieUpdateListener.php
+++ src/EventListener/MovieUpdateListener.php
@@ -5,6 +5,11 @@ namespace App\EventListener;
 use App\Entity\Movie;
 use Doctrine\Persistence\Event\LifecycleEventArgs;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 class MovieUpdateListener
 {
@@ -18,7 +23,13 @@ class MovieUpdateListener
     // the entity instance and the lifecycle event
     public function postPersist(Movie $movie, LifecycleEventArgs $event)
     {
-        $this->logger->info("Film : " . $movie->getTitle() . " updated");
+        $encoders = [new XmlEncoder(), new JsonEncoder()];
+        $normalizers = [new ObjectNormalizer()];
+
+        $serializer = new Serializer($normalizers, $encoders);
+        $jsonContent = $serializer->serialize($movie,   'json', [AbstractNormalizer::ATTRIBUTES => ['title','releaseDate','description','realisator']]);
+
+        $this->logger->info("Film updated : " . $jsonContent );
     }
 
 }
